37d05bcbac8f5f66587e4b8e3f1b7292e3046048
Di Lu <dilu@andrew.cmu.edu>
2021-06-29 03:34:19 -0400

    initialize repository structure

---
+++ src/main/java/com/cardcoupon/passbook/PassbookClientSideApplication.java
@@ -0,0 +1,13 @@
+package com.cardcoupon.passbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassbookClientSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassbookClientSideApplication.class, args);
+    }
+
+}
